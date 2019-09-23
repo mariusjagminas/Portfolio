fdcec5d974390594b709002258b95b02598bc94a
mariusjagminas <mariusjagminas.it@gmail.com>
2019-09-23 16:45:50 +0200

    Add header section

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,5 +1,11 @@
 const theme = {
   dark: "black",
+  white: "#ffffff",
+  color: "#e13726",
+  mq: {
+    tablet: "@media(min-width: 600px)",
+    laptop: "@media(min-width: 1050px",
+  },
 }
 
 export default theme
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,96 @@
 import React from "react"
 import styled from "styled-components"
 import Navigation from "../components/Navigation/Navigation"
+import { useStaticQuery, graphql } from "gatsby"
+import Background from "gatsby-background-image"
 
-const Section = styled.section`
+const BackgroundImage = styled(Background)`
   width: 100%;
   height: 100vh;
   position: relative;
-  background: green;
 `
+const Container = styled.div`
+  width: 100%;
+  max-width: 1260px;
+  margin: 0 auto;
+  padding-top: 44vh;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const Wrapper = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  font-size: 6.3px;
+  ${({ theme }) => theme.mq.tablet} {
+    font-size: 10px;
+    max-width: 500px;
+  }
+`
+
+const InnerWrapper = styled.div`
+  background-color: ${({ theme }) => theme.color};
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  padding: 1.2em 2em;
+`
+
+const H1 = styled.h1`
+  color: ${({ theme }) => theme.white};
+  font-size: 4em;
+  text-transform: uppercase;
+  margin: 0;
+`
+
+const P = styled.p`
+  padding: 0;
+  color: ${({ theme }) => theme.white};
+  font-size: 16px;
+  ${({ theme }) => theme.mq.tablet} {
+    font-size: 25px;
+  }
+`
+
 const Main = ({ isSticky }) => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
   return (
-    <Section id="main">
+    <BackgroundImage
+      id="main"
+      Tag="section"
+      fluid={[`linear-gradient(rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.8))`, fluid]}
+    >
+      <Container>
+        <Wrapper>
+          <InnerWrapper>
+            <H1>Marius Jagminas</H1>
+          </InnerWrapper>
+          <P>Front-end JavaScrip Developer. Working with React,Gatsby.</P>
+        </Wrapper>
+      </Container>
       <Navigation isSticky={isSticky} />
-    </Section>
+    </BackgroundImage>
   )
 }
 
 export default Main
+
+export const query = graphql`
+  query Main {
+    file(relativePath: { eq: "code.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`

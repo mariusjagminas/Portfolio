eef62fbe28a9f793f84c6a88027d03f9cf3c96ca
mariusjagminas <mariusjagminas.it@gmail.com>
2019-11-15 18:44:10 +0100

    Update projects list

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,15 +1,24 @@
+// Images size is 400 x 250 px.
+
 export const projects = [
   {
     title: "Contemporary literature",
     description:
-      "JAMstack multi-language website, built with Gatsby.js, Contentful Deployed on Netlify",
+      "JAMstack multi-language website, built with Gatsby.js, Contentful Deployed on Netlify.",
     imageName: "Contemporary-literature.jpg",
-    siteUrl: "https://mariusjagminas.github.io/JavaScript30/",
-    ghUrl: "https://github.com/mariusjagminas/JavaScript30",
+    siteUrl: "https://gallant-kare-5fe476.netlify.com/",
+    ghUrl: "https://github.com/mariusjagminas/Contemporary-literature",
+  },
+  {
+    title: "Tetris",
+    description: "A classic Tetris game built with React.",
+    imageName: "tetris.jpg",
+    siteUrl: "https://mariusjagminas.github.io/tetris/",
+    ghUrl: "https://github.com/mariusjagminas/tetris",
   },
   {
     title: "JavaScript30",
-    description: "A bunch of mini projects built with a plain JavaSript",
+    description: "A set of a 30 mini projects, built with plain JavaScript.",
     imageName: "java-script-30.jpg",
     siteUrl: "https://mariusjagminas.github.io/JavaScript30/",
     ghUrl: "https://github.com/mariusjagminas/JavaScript30",
@@ -17,7 +26,7 @@ export const projects = [
   {
     title: "Sniezka",
     description:
-      "The beautiful pictures of the mountain Sniezka. The website has a weather and location pages.",
+      "A landing page for Sniezka mountain. The website has a current weather page, also it uses a Google Maps API.",
     imageName: "sniezka.jpg",
     siteUrl: "https://mariusjagminas.github.io/Sniezka/",
     ghUrl: "https://github.com/mariusjagminas/Sniezka",
@@ -25,7 +34,7 @@ export const projects = [
   {
     title: "Top Designs",
     description:
-      ' "Pixel perfect" PSD to HTML exercise. Fictional landing page',
+      '"Pixel perfect" PSD to HTML exercise.Landing page for a fictional company.',
     imageName: "top-designs.jpg",
     siteUrl: "https://mariusjagminas.github.io/TopDesigns/",
     ghUrl: "https://github.com/mariusjagminas/TopDesigns",
@@ -33,15 +42,7 @@ export const projects = [
   {
     title: "TreeHouse",
     description:
-      ' "Pixel perfect" PSD to HTML exercise. Fictional landing page',
-    imageName: "treehouse.jpg",
-    siteUrl: "https://mariusjagminas.github.io/TreeHouse/",
-    ghUrl: "https://github.com/mariusjagminas/TreeHouse",
-  },
-  {
-    title: "TreeHousea",
-    description:
-      ' "Pixel perfect" PSD to HTML exercise. Fictional landing page',
+      '"Pixel perfect" PSD to HTML exercise.Landing page for a fictional company.',
     imageName: "treehouse.jpg",
     siteUrl: "https://mariusjagminas.github.io/TreeHouse/",
     ghUrl: "https://github.com/mariusjagminas/TreeHouse",
